479a2a26df79a22cdebc2e877de3ea1da0a5fa26
openplatform <openplatform@guardian.co.uk>
2009-03-10 01:04:06 +0000

    Removed incomplete appengine caching helper

--- guardianapi/appengine.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.api import memcache
-from fetchers import HttpCacheFetcher
-
-class AppEngineFetcher(HttpCacheFetcher):
-    def __init__(self):
-        super(AppEngineFetcher, self).__init__(memcache)
